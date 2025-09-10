ddfae539607fdb7cfc02aae59e911aece68118a4
devcui3107 <cudun6789@gmail.com>
2025-09-10 15:36:47 +0700

    Fixed Return Home By NotFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./NotFound.module.scss";
 
 function NotFound() {
@@ -7,9 +8,9 @@ function NotFound() {
       <p className={styles.notfoundDesc}>
         Oops! The page you are looking for does not exist.
       </p>
-      <a href="/" className={styles.notfoundBtn}>
-        Go Back Home
-      </a>
+      <div className={styles.notfoundBtn}>
+        <Link to="/">Go Back Home</Link>
+      </div>
     </div>
   );
 }
